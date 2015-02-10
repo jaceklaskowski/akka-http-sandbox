88a60addabdb08bfe6964e3fb3812f432b24406d
Jacek Laskowski <jacek@japila.pl>
2015-02-10 23:51:49 +0100

    ActorSubscriber under test

--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -1,3 +1,8 @@
+import akka.actor.{Props, ActorSystem, Actor}
+import akka.actor.Actor.Receive
+import akka.stream.ActorFlowMaterializer
+import akka.stream.actor.{WatermarkRequestStrategy, RequestStrategy, ActorSubscriber}
+import akka.stream.scaladsl.{Flow, Source, Sink}
 import org.specs2._
 
 class HelloSpec extends Specification { def is = s2"""
@@ -8,5 +13,23 @@ class HelloSpec extends Specification { def is = s2"""
     run a Akka Streams flow   $e1
 """
 
-  def e1 = todo
+  class MyActor extends ActorSubscriber {
+
+    override protected def requestStrategy: RequestStrategy = WatermarkRequestStrategy(10)
+
+    override def receive: Actor.Receive = {
+      case s: String => sender() ! "thanks"
+    }
+  }
+
+  def e1 = {
+    implicit val system = ActorSystem("reactive-tweets")
+    implicit val mat = ActorFlowMaterializer()
+
+    val subscriber = Props(new MyActor)
+
+    Source(0 to 3).map(_ * 2).runWith(Sink(subscriber))
+
+    success
+  }
 }
